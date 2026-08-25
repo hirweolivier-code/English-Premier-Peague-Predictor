d00fbad69348612aee6ee89d9de56e77f383d10c
hirweolivier-code <hirweolivier@gmail.com>
2026-08-25 15:08:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -626,7 +626,7 @@ if st.button("Predict Match"):
 
         with col_vs:
             st.markdown(
-                "<div style='text-align:center; "
+                "<div style='text-align:left; "
                 "font-size:30px; font-weight:700; "
                 "padding-top:35px;'>VS</div>",
                 unsafe_allow_html=True
